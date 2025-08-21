43f320c936ec230b906dbbd1424acd64f8295a51
Ansh-gupta02 <harshitgupta11005@gmail.com>
2025-08-21 23:30:06 +0530

    the project is ready

---
+++ server/middlewares/auth.js
@@ -0,0 +1,58 @@
+import { clerkClient } from "@clerk/express";
+
+// Middleware to check userId and hasPremiumPlan
+
+export const auth = async (req, res, next)=>{
+    try {
+        const {userId, has} = await req.auth();
+        const hasPremiumPlan = await has({plan: 'premium'});
+
+        const user = await clerkClient.users.getUser(userId);
+
+        if(!hasPremiumPlan && user.privateMetadata.free_usage){
+            req.free_usage = user.privateMetadata.free_usage
+        } else{
+            await clerkClient.users.updateUserMetadata(userId, {
+                privateMetadata: {
+                    free_usage: 0
+                }
+            })
+            req.free_usage = 0;
+        }
+
+        req.plan = hasPremiumPlan ? 'premium' : 'free';
+        next()
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+// Middleware to check userId and hasPremiumPlan
+// import { clerkClient } from "@clerk/express";
+
+// export const auth = async (req, res, next) => {
+//     try {
+//         // Correct way to access userId and has method
+//         const { userId, sessionId, has } = req.auth;
+//         const hasPremiumPlan = await has({ plan: 'premium' });
+
+//         const user = await clerkClient.users.getUser(userId);
+
+//         if (!hasPremiumPlan && user.privateMetadata.free_usage) {
+//             req.free_usage = user.privateMetadata.free_usage
+//         } else {
+//             await clerkClient.users.updateUserMetadata(userId, {
+//                 privateMetadata: {
+//                     free_usage: 0
+//                 }
+//             })
+//             req.free_usage = 0;
+//         }
+
+//         req.plan = hasPremiumPlan ? 'premium' : 'free';
+//         next();
+//     } catch (error) {
+//         res.status(401).json({ success: false, message: "Authentication failed." });
+//     }
+// };
